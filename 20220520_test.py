75ea125eee3fc2a7820d41e4e5465122a425f762
alstjrdld1 <minseok_kim@khu.ac.kr>
2022-05-21 22:43:38 +0900

    20220521

--- 20220520_test.py
+++ 20220520_test.py
@@ -22,7 +22,6 @@ def train(model_name, num_epochs):
 
   train_loader = torch.utils.data.DataLoader(dataset = train_dataset, batch_size = batch_size, shuffle=True)
   test_loader = torch.utils.data.DataLoader(dataset = test_dataset, batch_size = batch_size, shuffle=False)
-  scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, [75,125], gamma=0.1)
   # About Model
   if(model_name == "AE"):
     model = AutoEncoder(28*28, 64, 32)
@@ -34,6 +33,8 @@ def train(model_name, num_epochs):
   model = model.cuda()
   learning_rate = 0.01
   optimizer = optim.Adam(model.parameters(), lr = learning_rate)
+  scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, [75,125, 250, 500, 750], gamma=0.1)
+
   Loss = nn.MSELoss()
 
   train_loss_arr = []
